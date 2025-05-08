74f989ef06f27cf6302a40a37f8af8befc38c1c4
danielsousa <danielsousa@icligo.com>
2025-05-08 11:54:00 +0100

    feat: save changes when clicking on next step

--- apps/web/components/stepper.tsx
+++ apps/web/components/stepper.tsx
@@ -7,8 +7,13 @@ export function Stepper({ onContinue }: { onContinue: () => void }) {
   const pathname = usePathname();
   const currentPath = pathname.split("/").pop() || "";
   const router = useRouter();
-  const { formData, stepsConfig, isCurrentStepValid, currentStep } =
-    useFormSteps();
+  const {
+    formData,
+    stepsConfig,
+    isCurrentStepValid,
+    currentStep,
+    setCurrentStep,
+  } = useFormSteps();
 
   const isStepAllowed = (index: number) => {
     // If this is the next step, check if current one is valid in order to enable continuing throught the stepper
@@ -38,7 +43,12 @@ export function Stepper({ onContinue }: { onContinue: () => void }) {
             className={`px-4 py-2 ${currentPath === step.path ? "bg-primary text-black outline outline-green-300 cursor-not-allowed" : "bg-gray-200 cursor-pointer"} ${!isStepAllowed(index) ? "opacity-30 cursor-not-allowed" : ""}`}
             onClick={() => {
               if (isStepAllowed(index)) {
-                router.push(step.path);
+                setCurrentStep(index);
+                if (index > currentStep) {
+                  onContinue();
+                } else {
+                  router.push(step.path);
+                }
               }
             }}
           >
